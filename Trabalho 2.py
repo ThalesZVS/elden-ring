892c625443116e02536ba655328116a5f8b5831f
ThalesZVS <thales.venancio.santos@escola.pr.gov.br>
2025-10-03 08:50:10 -0300

    Update Trabalho 2.py

--- Trabalho 2.py
+++ Trabalho 2.py
@@ -1,5 +1,5 @@
 import requests
-def wiki_dos_bosses():
+def Wiki_dos_locais():
     Url = "https://eldenring.fanapis.com/api/locations"
     Response = requests.get(Url)
     print(Response)
@@ -10,7 +10,9 @@ def wiki_dos_bosses():
     Escolha = input("Escolha um local ")
     for local in locais:
         if Escolha == local["name"]:
-            print(local["description"])
+            print("Nome do local: " + local["name"])
+            print("Descrição do local: " + local["description"])
+            print("Região localizada: " + local["region"])
     print("Digite 1 para continuar o 0 para sair para o menu inicial.")
     Escolha2 = int(input())
     if Escolha2 == 1:
@@ -31,7 +33,7 @@ def wiki_dos_bosses():
           menu_inicial()
         else:
             print("Local não encontrado. Por favor, tente novamente.")
-def Wiki_dos_locais():
+def wiki_dos_bosses():
     Url = "https://eldenring.fanapis.com/api/bosses"
     Response = requests.get(Url)
     print(Response)
@@ -42,7 +44,11 @@ def Wiki_dos_locais():
     Escolha = input("Escolha um Boss ")
     for Bosses in Criaturas:
         if Escolha == Bosses["name"]:
-            print(Bosses["description"])
+            print("Nome da creatura: " + Bosses["name"])
+            print("Descrição da creatura: " + Bosses["description"])
+            print("localização da espécie: " + Bosses["location"])
+            print("Items dropados pela creatura: ")
+            print(Bosses["drops"])
             print("Digite 2 para continuar ou 0 para sair para o menu inicial.")
             Escolha2 =int(input())
             if Escolha2 == 2:
@@ -77,9 +83,9 @@ def menu_inicial():
         escolha = input("Escolha uma funçâo (1/2/3/4/5/6/0): ")
 
         if escolha == "1":
-           wiki_dos_bosses()
-        elif escolha == "2":
            Wiki_dos_locais()
+        elif escolha == "2":
+           wiki_dos_bosses()
         elif escolha == "3":
             wiki_das_criaturas()
         elif escolha == "4":
@@ -105,7 +111,11 @@ def wiki_das_criaturas():
     Escolha = input("Escolha uma Criatura ")
     for especime in especies:
         if Escolha == especime["name"]:
-            print(especime["description"])
+            print("Nome da creatura: " + especime["name"])
+            print("Descrição da creatura: " + especime["description"])
+            print("localização da espécie: " + especime["location"])
+            print("Items dropados pela creatura: ")
+            print(especime["drops"])
 
             print("Digite 3 para continuar ou 0 para sair para o menu inicial.")
             Escolha2 =int(input())
@@ -133,7 +143,9 @@ def wiki_dos_npcs():
     Escolha = input("Escolha um npc ")
     for npc in Npcs:
         if Escolha == npc["name"]:
-            print(npc["role"])
+            print("Nome: " + npc["name"])
+            print("Função no mundo: " + npc["role"])
+            print("Localização: " + npc["location"])
             print("Digite 4 para continuar ou 0 para sair para o menu inicial.")
             Escolha2 =int(input())
             if Escolha2 == 4:
@@ -159,7 +171,10 @@ def wiki_da_armas():
     Escolha = input("Escolha uma arma ")
     for arma in armas:
         if Escolha == arma["name"]:
-            print(arma["description"])
+            print ("Nome da classe: " + arma["name"])
+            print("Descrisão da Arma: " + arma["description"])
+            print("Estatísticas da arma: ")
+            print(arma["attack"])
             print("Digite 5 para continuar ou 0 para sair para o menu inicial. ")
             Escolha2 =int(input())
             if Escolha2 == 5:
@@ -185,7 +200,9 @@ def wiki_das_classes():
     Escolha = input("Escolha uma classe ")
     for classe in classes:
         if Escolha == classe["name"]:
-            print(classe["description"])
+            print ("Nome da classe: " + classe["name"])
+            print("Descrisão da classe: " + classe["description"])
+            print("Estatísticas da classe: ")
             print(classe["stats"])
             print("Digite 6 para continuar ou 0 para sair para o menu inicial. ")
             Escolha2 =int(input())
@@ -204,38 +221,6 @@ def wiki_das_classes():
 # Execução principal
 if __name__ == "__main__":
     menu_inicial()
-
-    #codigo perfeito onde ele entrega os locais e para quando nao tem mais oq entregar sem erro.
-    #print(Dados)
-    #print(Dados["data"][2]["name"]) #se eu quiser de todos eu faço um loppy
-    
-    #A = 1
-    #while True:
-    #print(Dados["data"][A]["name"])
-    #A =A+1 #assim ele vai funcionar mais vai falar que esta errado pois vai ir ate falhar. portanto vc tem que fazer uma continuaçao para que ele tenha um limite.
-    #nesse momento estou tentando colocar para se a pessoa escrever errado o o codigo fazer um loop ate a pessoa acertar. o professor
-    #falou um jeito usand
-    #"def localizar()"
-    #A = imput 
-        #for
-            #if
-    #localizar()
-    #na logica essa seria uma estrutura legal... mas eu quero na minha cabeça teimosa eu poderia usar o while
-   
-
-    #codigo perfeito onde ele entrega os locais e para quando nao tem mais oq entregar sem erro.
-    #print(Dados)
-    #print(Dados["data"][2]["name"]) #se eu quiser de todos eu faço um loppy
-    
-    #A = 1
-    #while True:
-    #print(Dados["data"][A]["name"])
-    #A =A+1 #assim ele vai funcionar mais vai falar que esta errado pois vai ir ate falhar. portanto vc tem que fazer uma continuaçao para que ele tenha um limite.
-    #nesse momento estou tentando colocar para se a pessoa escrever errado o o codigo fazer um loop ate a pessoa acertar. o professor
-    #falou um jeito usand
-    #"def localizar()"
-    #A = imput 
-        #for
-            #if
-    #localizar()
-    #na logica essa seria uma estrutura legal... mas eu quero na minha cabeça teimosa eu poderia usar o while
+    #ir  atras de fazer uma nova funçao onde se tem uma builds prontas e uma forma de o sistema fazer uma build aleatoria... como seria muito desbalanceado da pra
+    #a build que voce recebesse aleatoria poderia tem algum buff desde drop ou loot
+    #e ir atras de guarda-la em um arquivo txt com aquele codigo que o professor passou... mas talves nao.
